8c16eaa75fe8de3dc8da5a52c9de3cdc81f09b1b
BrandenJoe <147530777+BrandenJoe@users.noreply.github.com>
2024-11-04 17:28:29 -0600

    update
    update

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
 const Login = () => {
   return (
     <div className='login'>
-      
+      <img src={logo} className ="login_logo" alt="" />
+      <div className='login_form'>
+        <h1>Sign In</h1>
+        <form>
+          <input type="text" />
+        </form>
+      </div>
     </div>
   )
 }
